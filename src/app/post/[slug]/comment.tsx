e10ab1ad8d772b7bd985e62d858af7a6dfdafe0d
MoKoCoBall <vngkgk1004@gmail.com>
2025-04-26 02:44:38 +0900

    qa

--- src/app/post/[slug]/comment.tsx
+++ src/app/post/[slug]/comment.tsx
@@ -1,28 +1,28 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useTheme } from "next-themes";
 
 export default function Comments() {
   const { theme } = useTheme();
-  const [currentTheme, setCurrentTheme] = useState("github-light");
 
+  // Apply Utterances
   useEffect(() => {
-    const newTheme = theme === "dark" ? "github-dark" : "github-light";
-    setCurrentTheme(newTheme);
-
     const script = document.createElement("script");
     script.src = "https://utteranc.es/client.js";
     script.setAttribute("repo", "MoKoCoBall/blog-comment");
     script.setAttribute("issue-term", "pathname");
-    script.setAttribute("theme", newTheme);
+    script.setAttribute(
+      "theme",
+      theme === "dark" ? "github-dark" : "github-light"
+    );
     script.setAttribute("label", "Comment");
     script.crossOrigin = "anonymous";
     script.async = true;
 
     const commentDiv = document.getElementById("comments");
     if (commentDiv) {
-      commentDiv.innerHTML = ""; // 기존 스크립트 제거
+      commentDiv.innerHTML = "";
       commentDiv.appendChild(script);
     }
   }, [theme]);
