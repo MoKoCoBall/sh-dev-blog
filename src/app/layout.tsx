bdb9e95347bca5ed32a3e7e2738e44207ee19327
MoKoCoBall <vngkgk1004@gmail.com>
2025-05-09 17:13:59 +0900

    Implement google adsnese

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,11 +40,12 @@ export default function RootLayout({
   return (
     <html lang="ko" suppressHydrationWarning>
       <head>
-        <script
+        <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1816289569308799"
           crossOrigin="anonymous"
-        ></script>
+          strategy="afterInteractive"
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${jetbrainsMono.variable}`}
